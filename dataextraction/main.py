7701f1719caf9e705ad6abc10331b074f701bcaa
michalzoldak97 <michalzoldak97@gmail.com>
2023-11-12 00:06:47 +0200

    analyze weapons df

--- dataextraction/main.py
+++ dataextraction/main.py
@@ -12,13 +12,23 @@ def _load_raw_html(url: str, target_pth: str):
     with open(target_pth, 'a') as f:
         f.write(str(r.content))
 
+def analyze_df(df: pd.DataFrame):
+    print(df.loc[df['content'].notna()].head()) # show rows where content column is not na
+    print(df['rarity'].unique())
+    df = df.loc[df['content'].notna()]
+    print(df.loc[df['content']
+                 .str.contains('base')]
+                 .groupby('manufacturer')
+                 .count()
+                 .sort_values('rarity'))
+    print(df.describe())
+
 
 def main():
     # _load_raw_html('https://www.lootlemon.com/db/borderlands-3/weapons?3493d0c0_page=4',
     #                './data/weapons-source.txt')
     wd_ex = wd.WeaponDataExtractor('./data/weapons-source.txt')
-    df = wd_ex.get_df()
-    print(df.head())
+    analyze_df(wd_ex.get_df())
 
 
 if __name__ == "__main__":
