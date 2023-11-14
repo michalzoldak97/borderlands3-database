f45acd2d1408839af14c9e52d1eb86bfd68dbaeb
Michał Żołdak <michalzoldak97@gmail.com>
2023-11-14 01:01:06 +0100

    find incindiary ar

--- dataextraction/main.py
+++ dataextraction/main.py
@@ -12,23 +12,37 @@ def _load_raw_html(url: str, target_pth: str):
     with open(target_pth, 'a') as f:
         f.write(str(r.content))
 
-def analyze_df(df: pd.DataFrame):
-    print(df.loc[df['content'].notna()].head()) # show rows where content column is not na
+
+def analyze_w_df(df: pd.DataFrame):
+    print(df.loc[df['content'].notna()].head())  # show rows where content column is not na
     print(df['rarity'].unique())
     df = df.loc[df['content'].notna()]
     print(df.loc[df['content']
-                 .str.contains('base')]
-                 .groupby('manufacturer')
-                 .count()
-                 .sort_values('rarity'))
+          .str.contains('base')]
+          .groupby('manufacturer')
+          .count()
+          .sort_values('rarity'))
     print(df.describe())
+
+
+def analyze_w_df_inc(df: pd.DataFrame):
+    df = df.loc[df['content'].notna()]
+    base_df = df.loc[df['content'].str.contains('base')].copy()
+    pd.options.display.max_columns = None
+    print(base_df.describe())
+    rarities = ['f-legendary', 'd-epic']
+    w_type = 'assault-rifle-bl3'
+    print(base_df.loc[base_df['elements']
+          .apply(lambda elements: len(elements) == 1 and 'incindiary' in elements)]
+          .query("rarity in @rarities")
+          .query("type == @w_type"))
 
 
 def main():
     # _load_raw_html('https://www.lootlemon.com/db/borderlands-3/weapons?3493d0c0_page=4',
     #                './data/weapons-source.txt')
     wd_ex = wd.WeaponDataExtractor('./data/weapons-source.txt')
-    analyze_df(wd_ex.get_df())
+    analyze_w_df_inc(wd_ex.get_df())
 
 
 if __name__ == "__main__":
